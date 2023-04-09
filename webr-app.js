664ce3240b4a5d957eb64ff5c02683043a4cd364
Danielle Navarro <djnavarro@protonmail.com>
2023-04-10 09:31:40 +1000

    simplifies comments

--- webr-app.js
+++ webr-app.js
@@ -12,16 +12,15 @@ import('https://webr.r-wasm.org/latest/webr.mjs').then(async ({WebR}) => {
     let automaton = await fetch('automaton.R');
     await webr.evalR(await automaton.text());
 
-    // initialise the grid string by calling automaton() with no input,
-    // pull results into JS, and update the display
+    // initialise the state of the grid
     let str = await webr.evalR('automaton()')
     grid.innerHTML = (await str.toJs()).values;
 
     // function to update the state of the grid
     async function grid_update() {
-        await webr.objs.globalEnv.bind('str', grid.innerHTML) // copy grid string to R
-        let str = await webr.evalR('automaton(str)')          // evaluate R function
-        grid.innerHTML = (await str.toJs()).values;           // update grid string
+        await webr.objs.globalEnv.bind('str', grid.innerHTML)
+        let str = await webr.evalR('automaton(str)')
+        grid.innerHTML = (await str.toJs()).values;
     }
 
     // repeatedly call the update function
